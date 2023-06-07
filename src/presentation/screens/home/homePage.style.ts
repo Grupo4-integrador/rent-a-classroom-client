76b7a8a825f080a79668d1643c99ddd7286b6c51
ppedroandrade <pedrosalvaro09@gmail.com>
2023-06-07 14:12:12 -0300

    home feita

--- src/presentation/screens/home/homePage.style.ts
+++ src/presentation/screens/home/homePage.style.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
+import backgroundImg from "../../../components/assets/fundo-tela-login.png";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url(../../../components/assets/fundo-tela-login.png);
+  background-image: url(${backgroundImg});
   background-repeat: no-repeat;
   background-position: center center;
   background-attachment: fixed;
@@ -12,12 +13,11 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
 
+`;
 export const Box = styled.div`
   width: 35%;
-  height: 70%;
-  background-image: linear-gradient(to top, white 81%, white 1%, #eeebeb 18%);
+  background-color: white;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
   flex-direction: column;
@@ -25,27 +25,38 @@ export const Box = styled.div`
   align-items: center;
   justify-content: center;
   gap: 10px;
-  
+  position: absolute;
+  border-radius: 10px;
   @media (max-width: 1200px) {
     width: 45%;
   }
-  
   @media (max-width: 992px) {
     width: 55%;
   }
   @media (max-width: 768px) {
     width: 75%;
   }
-  
-  
-  @media (min-width: 320px) and (max-width:560px) {
+  @media (min-width: 320px) and (max-width: 560px) {
     width: 90%;
   }
 `;
+
+export const HeaderBox = styled.div`
+  background-color: #eeebeb;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px 10px 0px 0px;
+
+`;
+
 export const Img = styled.img`
   display: flex;
   width: 100%;
-
+  transform: translate(-50% -50%);
+  top: 0;
 `;
 
 export const Title = styled.p`
@@ -56,19 +67,56 @@ export const Title = styled.p`
   font-weight: 700;
   font-size: 20px;
   color: #686868;
-
 `;
 
 export const InputGroup = styled.div`
   font-family: "Segoe UI", sans-serif;
-  padding: 10px;
+  position: relative;
+  .teste:hover{
+    border: 0.5px solid rgb(00, 00, 00);
+  }
+  .teste:focus{
+    border: 1px solid #0000ee;
+  }
+  
+`;
+
+export const ButtonGroup = styled.div`
+  .login-bt {
+    background-color: rgb(1, 231, 142);
+    color: #fff;
+    border: none;
+    display: flex;
+    justify-content: center;
+    font-size: 20;
+    line-height: 1.5;
+    font-family: sans-serif;
+    cursor: pointer;
+
+  }
+  .login-bt:hover{
+    background-color:#0000ee;
+    transition: all 0.6s ease;
+  }
+  .forgetPassword-bt {
+    display: flex;
+    border: solid 0.5px #0000ee;
+    color: #0000ee;
+    justify-content: center;
+    cursor: pointer;
+  }
+  .forgetPassword-bt:hover{
+    background-color:rgba(0, 0, 238, 0.8);
+    color:white;
+    transition: all 0.6s ease;
+  }
 `;
 export const Input = styled.input`
   font-size: 100%;
   width: 30vw;
   height: 45px;
   outline: none;
-  border: 2px solid rgb(200, 200, 200);
+  border: 0.5px solid rgb(200, 200, 200);
   background-color: transparent;
   border-radius: 5px;
   display: flex;
@@ -77,43 +125,48 @@ export const Input = styled.input`
   @media (max-width: 1200px) {
     width: 40vw;
   }
-  
+
   @media (max-width: 992px) {
     width: 45.5vw;
   }
   @media (max-width: 768px) {
     width: 45.5vw;
   }
-  
-  
-  @media (min-width: 320px) and (max-width:560px) {
+
+  @media (min-width: 320px) and (max-width: 560px) {
     width: 70vw;
   }
+
 `;
 
-export const ButtonGroup = styled.div`
-  font-family: "Segoe UI", sans-serif;
-  padding: 10px;
+export const Label = styled.label`
+  position: absolute;
+
+  top: 24px;
+  left: 20px;
+  background: transparent;
+
+
+  ${Input}:focus ~ & {
+   top: 0px;
+   left: 30px;
+   background: white;
+   color: #0000ee;
+  }
+  ${Input}:placeholder-shown + & {
+    top: 0px;
+    left: 30px;
+    background: white;
+    color: #686868;
+  }
+ pointer-events: none;
+ transition: all 0.3s ease;
 `;
 
-export const Button = styled.button`
-      font-size: 100%;
-  width: 30vw;
-  height: 45px;
-  border-radius: 5px;
-  margin: 10px;
-  display: flex;
-
-  @media (max-width: 1200px) {
-    width: 40vw;
-  }
-  @media (max-width: 992px) {
-    width: 45.5vw;
-  }
-  @media (max-width: 768px) {
-    width: 45.5vw;
-  }
-  @media (min-width: 320px) and (max-width:560px) {
-    width: 70vw;
-  }
+export const Imgopacity = styled.div`
+    opacity: 0.6;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background: #fff;
 `;
--- src/presentation/screens/home/homePage.tsx
+++ src/presentation/screens/home/homePage.tsx
@@ -1,18 +1,52 @@
-import { Container, Box, InputGroup, Input, Button, ButtonGroup, Title, Img } from "./homePage.style";
-import Logo from '../../../components/assets/logo.png';
+import React, { useState } from "react";
+
+import {
+  Container,
+  Box,
+  InputGroup,
+  Input,
+  Title,
+  Img,
+  HeaderBox,
+  ButtonGroup,
+  Label
+} from "./homePage.style";
+import Logo from "../../../components/assets/logo.png";
+
 export function HomePage() {
+  const [loginClicked, setLoginClicked] = useState(false);
+
+  const handleLoginClick = () => {
+    setLoginClicked(true);
+    alert("Botão de login clicado!");
+  };
   return (
     <Container>
       <Box>
-        <Img src={Logo}></Img>
-        <Title>Login - Sistemas de Reservas de Espaços</Title>
+        <HeaderBox>
+          <Img src={Logo}></Img>
+        </HeaderBox>
+        <Title>Sistemas de Reservas de Espaços</Title>
         <InputGroup>
-          <Input type="text" placeholder="Informe seu Login" />
-          <Input type="text" placeholder="Insira sua Senha" />
+          <Input type="text" className="teste" placeholder=""/>
+          <Label>Informe seu Login</Label>
+        </InputGroup>
+        <InputGroup>
+          <Input type="password"  className="teste" placeholder=""/>
+          <Label>Insira sua Senha</Label>
         </InputGroup>
         <ButtonGroup>
-        <Button>Login</Button>
-        <Button>Esqueci minha senha</Button>
+          <Input
+            type="button"
+            className="login-bt"
+            value="Login"
+            onClick={handleLoginClick}
+          />
+          <Input
+            type="button"
+            className="forgetPassword-bt"
+            value="Esqueci a senha"
+          ></Input>
         </ButtonGroup>
       </Box>
     </Container>
