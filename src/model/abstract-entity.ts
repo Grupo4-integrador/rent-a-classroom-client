59f78490d733a7475ef3b51e218b9a5035e75dd8
gustavoemf <gustavoeduardomf@gmail.com>
2023-06-22 20:33:51 -0400

    Situacao & RoleUsuario Enum + AbstractEntity

--- src/model/abstract-entity.ts
+++ src/model/abstract-entity.ts
@@ -1,13 +1,13 @@
 export class AbstractEntity {
 	id!: number;
-    cadastro! : Date;
-    atualizacao! : Date;
-    ativo! : boolean;
+	cadastro!: Date;
+	atualizacao!: Date;
+	ativo!: boolean;
 
-    constructor() {
-        this.id;
-        this.cadastro = new Date;
-        this.atualizacao = new Date;
-        this.ativo = true;
-    }
+	constructor() {
+		this.id;
+		this.cadastro = new Date();
+		this.atualizacao = new Date();
+		this.ativo = true;
+	}
 }
