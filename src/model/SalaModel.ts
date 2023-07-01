6e8b8e0496b5986656522b4765ac1ac7b20db376
gustavoemf <gustavoeduardomf@gmail.com>
2023-07-01 13:49:11 -0400

    Model 4

---
+++ src/model/SalaModel.ts
@@ -0,0 +1,8 @@
+import { AbstractEntity } from './AbstractEntityModel';
+import { Andar } from './enum/AndarModel';
+
+export class Sala extends AbstractEntity {
+    nome!: string;
+    capacidade!: number;
+    andar!: Andar;
+}
